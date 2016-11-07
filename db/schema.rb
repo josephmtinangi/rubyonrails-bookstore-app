4c63a7aecb40c80afa0fdd75156be7f3840ca80f
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-07 15:12:37 +0300

    Add coverpath field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107101327) do
+ActiveRecord::Schema.define(version: 20161107120847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20161107101327) do
     t.integer  "pages"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "url"
+    t.string   "coverpath"
   end
 
   create_table "categories", force: :cascade do |t|
