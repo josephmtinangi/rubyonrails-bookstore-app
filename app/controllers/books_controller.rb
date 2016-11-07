4235f70edb3f11498124c384e509887c27de61dc
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-07 13:28:59 +0300

    Add book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,4 +18,9 @@ class BooksController < ApplicationController
 
   def delete
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :category_id, :author_id, :publisher_id, :isbn, :price, :buy, :format, :excerpt, :pages, :year)
+  end
 end
