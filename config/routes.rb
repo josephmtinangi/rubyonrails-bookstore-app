ea8af7da411432292e1be7de12454b0e2be80da0
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-07 08:44:10 +0300

    Generate Books controller and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'books/new'
+
+  get 'books/edit'
+
+  get 'books/delete'
+
   get 'about/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
